140a033943fc264865666171642e38498aaf48d0
metamorpic-rock <rcrocamora05@gmail.com>
2023-10-23 04:14:01 +0800

    added the product services

---
+++ src/Biahs-Jewels/BiahsJewels.Mvc/Services/ProductServices.cs
@@ -0,0 +1,50 @@
+﻿using BiahsJewels.Mvc.Data;
+using BiahsJewels.Mvc.Models;
+
+namespace BiahsJewels.Mvc.Services;
+
+public interface IProductService
+{
+    public List<Product> GetProducts();
+    public Product GetProductById(int id);
+    public Product GetProductByName(string name);
+    public void SaveProduct(Product product);
+    public void EditProduct(Product product);
+    public void DeleteProduct(int id);
+}
+public class ProductServices : IProductService
+{
+    private readonly AppDbContext _appDbContext;
+
+    public ProductServices (AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+    public List<Product> GetProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product GetProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product GetProductByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+    public void EditProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+    public void DeleteProduct(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
