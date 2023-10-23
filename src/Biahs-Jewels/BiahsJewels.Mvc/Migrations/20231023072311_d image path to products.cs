4381c92a66f4528b838113449d5fcb1f678bb8c7
metamorpic-rock <rcrocamora05@gmail.com>
2023-10-23 15:34:14 +0800

    add migrations and update createProductModal

---
+++ src/Biahs-Jewels/BiahsJewels.Mvc/Migrations/20231023072311_d image path to products.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiahsJewels.Mvc.Migrations
+{
+    public partial class dimagepathtoproducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "ProductItem",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "ProductItem");
+        }
+    }
+}
